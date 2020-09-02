55efc101dbaf066d1b4ac615a9b5ddc57ca8e2d2
Yang-Jiangfeng <13551490760@163.com>
2020-09-02 10:01:08 +0800

    test提交

---
+++ test_one/python_base.py
@@ -0,0 +1,17 @@
+__author__="yang";
+'''
+python base demo
+'''
+
+# ====输入输出====
+print("请输入：")
+#a=input();
+#print(a);#输入
+a=11;
+b="xx";
+print(a);
+print("hello word");
+print("hello","word");#逗号自带空格
+print("hello %s"%a);#占位符%s
+print("hello %s"%(a,));#括号加逗号效果一致
+print("hello %s - %s"%(a,b));#不用-也行
